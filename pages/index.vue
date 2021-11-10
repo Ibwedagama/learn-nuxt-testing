c382322a5b34f786d8925fc1c4905ac69f61a9b0
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-10 20:23:28 +0700

    chore (home): mount card component on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <Tutorial/>
+  <main class="max-w-screen-lg mx-auto pt-20">
+    <Card
+      title="My Custom Card"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sapiente corrupti corporis sed aspernatur! Facere."
+      image="https://picsum.photos/600"
+    />
+  </main>
 </template>
 
 <script>
-export default {}
+import Card from '../components/Card'
+
+export default {
+  components: {
+    Card,
+  },
+}
 </script>
