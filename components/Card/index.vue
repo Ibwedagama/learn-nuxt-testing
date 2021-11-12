b52a5a1e80cef46639c2f937c5de45d0c136fa26
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-12 10:33:54 +0700

    fix (card): change id props to key

--- components/Card/index.vue
+++ components/Card/index.vue
@@ -39,7 +39,7 @@
 export default {
   name: 'Card',
   props: {
-    id: {
+    key: {
       type: [String, Number],
       required: true
     },
@@ -64,7 +64,7 @@ export default {
   methods: {
     toggleFavorite() {
       this.favorite = !this.favorite
-      this.$emit('favorite', this.id)
+      this.$emit('favorite', this.key)
     },
   },
 }
