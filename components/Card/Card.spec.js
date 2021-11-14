3b4d4a2ef17ce7a510229e7449d7e2b9689dd41e
Ibwedagama <wardana.wedagama@gmail.com>
2021-11-14 10:10:43 +0700

    chore (test): add example of implementation detail testing

--- components/Card/Card.spec.js
+++ components/Card/Card.spec.js
@@ -3,7 +3,10 @@ import Card from './'
 
 describe('Component: Card', () => {
 
-  test('should render an image based on props', () => {
+  // WHY WE MUST AVOID IMPLEMENTATION DETAIL TESTING!
+
+  test('should mutate `favorite` state when `toggleFavorite` method is called', async () => {
+
     const wrapper = mount(Card, {
       propsData: {
         cardId: 1,
@@ -13,81 +16,15 @@ describe('Component: Card', () => {
       }
     })
 
-    const image = wrapper.findComponent({ ref:  'card-image' })
+    // call toggleFavorite method
+    await wrapper.vm.toggleFavorite()
 
-    expect(image.exists()).toBe(true)
-    expect(image.attributes('src')).toContain('test-image.jpg')
+    expect(wrapper.vm.favorite).toBe(true)
+
+    // call toggleFavorite method again
+    await wrapper.vm.toggleFavorite()
+
+    expect(wrapper.vm.favorite).toBe(false)
   })
-
-  test('should render title based on props', () => {
-    const wrapper = mount(Card, {
-      propsData: {
-        cardId: 1,
-        title: 'my card title',
-        description: 'lorem ipsum',
-        image: 'test-image.jpg'
-      }
-    })
-
-    const title = wrapper.findComponent({ ref: 'card-title' })
-    expect(title.text()).toMatch(/my card title/i)
-  })
-
-  test('should render description based on props', () => {
-    const wrapper = mount(Card, {
-      propsData: {
-        cardId: 1,
-        title: 'my card title',
-        description: 'lorem ipsum',
-        image: 'test-image.jpg'
-      }
-    })
-
-    const description = wrapper.findComponent({ ref: 'card-description' })
-    expect(description.text()).toMatch(/lorem ipsum/i)
-  })
-
-  test('should change button label and color when button is clicked', async () => {
-    const wrapper = mount(Card, {
-      propsData: {
-        cardId: 1,
-        title: 'my card title',
-        description: 'lorem ipsum',
-        image: 'test-image.jpg'
-      }
-    })
-
-    const button = wrapper.findComponent({ ref: 'card-button' })
-
-    expect(button.text()).toMatch(/add to favorite/i)
-
-    // click button
-    await button.trigger('click')
-
-    // should be change label now
-    expect(button.text()).toMatch(/remove from favorite/i)
-
-    // should be change color now
-    expect(button.classes()).toContain('card__button--favorite')
-  })
-
-  test('should emit an event when favorite button is clicked', async () => {
-    const wrapper = mount(Card, {
-      propsData: {
-        cardId: 1,
-        title: 'my card title',
-        description: 'lorem ipsum',
-        image: 'test-image.jpg'
-      }
-    })
-
-    const button = wrapper.findComponent({ ref: 'card-button' })
-
-    await button.trigger('click')
-
-    expect(wrapper.emitted().favorite).toBeTruthy()
-    expect(wrapper.emitted().favorite[0]).toEqual([1])
-  })
-
 })
 
